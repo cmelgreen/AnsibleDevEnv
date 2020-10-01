9b3466854763ff8bec6a84f22ac43e93902c06df
Cooper Melgreen <cmelgreen@gmail.com>
2020-09-30 23:16:43 -0500

    cleanup files

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,14 @@
 sudo apt update && sudo apt -y upgrade
 sudo apt install -y ansible
+
 mv AnsibleDevEnv/setup.yml ~/
 ansible-playbook setup.yml
+rm setup.yml
+
 . .bash_profile
+
 mv AnsibleDevEnv/buildpipeline.yml ~/
 ansible-playbook buildpipeline.yml
+rm buildpipeline.yml
+
+rm -rf AnsibleDevEnv
