f4e982cc49f25a83838725ed03e0f5e355f79bee
Cooper Melgreen <cmelgreen@gmail.com>
2020-09-30 23:33:30 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,6 +11,6 @@ rm setup.retry
 mv AnsibleDevEnv/buildpipeline.yml ~/
 ansible-playbook buildpipeline.yml
 rm buildpipeline.yml
-rm buildpipeline.yml
+rm buildpipeline.retry
 
 rm -rf AnsibleDevEnv
