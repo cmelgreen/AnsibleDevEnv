95124aa6ec524d4fe9da7bfc0423faee82302f91
Cooper Melgreen <cmelgreen@gmail.com>
2021-01-22 14:36:50 -0600

    add github key gen

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,9 @@
+# get email, assume github username is same and split it off from domain
+echo Email for github?
+read -r email
+name=$(echo "$email" | grep -o '^[^@]*')
+
+# Setup basic development env
 cd ~
 
 sudo apt update && sudo apt -y upgrade
@@ -10,9 +16,20 @@ rm setup.retry
 
 . .bash_profile
 
+# Add infrastructure as code tools
 mv AnsibleDevEnv/buildtools.yml ~/
 ansible-playbook buildtools.yml
 rm buildtools.yml
 rm buildtools.retry
 
 rm -rf AnsibleDevEnv
+
+#setup github
+git config --global user.name "$name"
+git config --global user.email "$email"
+git config --global color.ui true
+
+yes "" | ssh-keygen -t rsa -C "$email"
+cat "$HOME"/.ssh/id_rsa.pub
+
+rm -rf ./SetupDebianDevEnv
